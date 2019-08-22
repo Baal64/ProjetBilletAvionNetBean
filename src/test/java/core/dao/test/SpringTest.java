18d32333245dc7200fcd37c3cd44077e349ac787
Alexandre Ba <alexandre64.ba@gmail.com>
2019-08-22 15:17:37 +0200

    create: dao crud repo

--- src/test/java/core/dao/test/SpringTest.java
+++ src/test/java/core/dao/test/SpringTest.java
@@ -5,13 +5,19 @@
  */
 package core.dao.test;
 
-import core.dao.UtilisateurDAO;
+import core.dao.PassagerDAOCrudRepository;
+import core.dao.ReservationDAOCrudrepository;
 import core.dao.UtilisateurDAOCrudRepository;
+import core.dao.VolDAOCrudRepository;
+import core.entity.Passager;
+import core.entity.Reservation;
 import core.entity.Utilisateur;
+import core.entity.Vol;
 import core.spring.SpringConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,13 +28,14 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
+@Rollback(false)
+@Transactional
 public class SpringTest {
     
     @Autowired
     private UtilisateurDAOCrudRepository dao;
     
     @Test
-    @Transactional
     public void createUtilisateurOK() {
         
         Utilisateur u = new Utilisateur("machin", "machin", "machin", "machin", "machin", "machin", "machin", "machin");
@@ -36,5 +43,31 @@ public class SpringTest {
         dao.save(u);
     }
     
+    @Autowired
+    private PassagerDAOCrudRepository passagerDao;
     
+    @Test
+    public void createPassagerOK() {
+        Passager p = new  Passager("nom", "prenom", "nuemroPlace");
+        passagerDao.save(p);
+    }
+    
+    @Autowired
+    private VolDAOCrudRepository volDAO;
+    
+    @Test
+    public void createVolOK(){
+        Vol v = new Vol("numeroVol", "villeDepart", "villeArrivee");
+        volDAO.save(v);
+    }
+    @Autowired
+    private ReservationDAOCrudrepository reservationDAO;
+    
+    @Test
+    public void createReservationOK(){
+        
+        Reservation r = new Reservation("13");
+        reservationDAO.save(r);
+    }
 }
+   
