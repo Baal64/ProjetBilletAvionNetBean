6b5e6b90f3000bc0b9fc0c99c8afc204edeb19b4
Alexandre Ba <alexandre64.ba@gmail.com>
2019-08-23 11:05:04 +0200

    create: service utilisateur

---
+++ src/main/java/core/service/UtilisateurService.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core.service;
+
+import core.dao.UtilisateurDAOCrudRepository;
+import core.entity.Utilisateur;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author alexa
+ */
+@Transactional
+@Service
+public class UtilisateurService {
+    @Autowired
+    private UtilisateurDAOCrudRepository utilisateurDao;
+    
+    public void create(Utilisateur u){
+          
+            utilisateurDao.save(u);
+    }
+    
+    public void modify(Utilisateur u){
+        
+        utilisateurDao.save(u);
+    }
+    
+    public void delete(Utilisateur u){
+        
+        utilisateurDao.delete(u);
+    }
+    
+    public Utilisateur read(Long l) {
+        return null;
+           
+    }
+}
